67c9a5806c54106f460e4174b9c9fabe1a722224
akhilreddy6g <akhilreddyg21pa@gmail.com>
2024-08-11 23:53:38 -0400

    Website Navbar Update-1

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Header(){
     return <nav className="navbar navbar-expand-lg bg-body-tertiary">
     <div className="container-fluid">
-        <img className="logo" id="menulogo" src="src/assets/menu.png" alt="Menu logo" />
-        <img className="logo" id="timelogo" src="src/assets/time-logo.png" alt="App logo" />
+        <a href="/review"><img className="logo" id="menulogo" src="src/assets/menu-logo.png" alt="Menu logo" /></a>
+        <a href="/daily-activities"> <img className="logo" id="dailyactlogo" src="src/assets/daily-activities-logo.png" alt="DAct logo" /></a>
         <div className="title-container" id="titlecontainer">
         <a className="navbar-brand" id="titlelink" href="/"><p id="titlename">TIMEQUEST</p></a>
         </div>
