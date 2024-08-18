be8b694b438ea1203680fcace60ecaf33e92bc9f
akhilreddy6g <akhilreddyg21pa@gmail.com>
2024-08-17 22:16:34 -0400

    Update Title

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header(){
       <img className="logo" id="menulogo" src="src/assets/menu.png" alt="Menu logo" onClick={toggleData}/>
       <a href="/daily-activities"> <img className="logo" id="dailyactlogo" src="src/assets/daily-activities.png" alt="DAct logo" /></a>
       <div className="title-container" id="titlecontainer">
-        <a className="navbar-brand" id="titlelink" href="/"><p id="titlename">TIMEQUEST</p></a>
+        <a className="navbar-brand" id="titlelink" href="/"><p id="titlename">Workspace</p></a>
       </div>
       <a href="/day-off"><img className="logo" id="powerlogo" src="src/assets/white-power.png" alt="Power logo"/></a>
     </div>
