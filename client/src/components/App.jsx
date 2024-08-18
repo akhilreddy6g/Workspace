4758e7ce0f16a9838ce41c16c7a8c483c9cc0fdb
akhilreddy6g <akhilreddyg21pa@gmail.com>
2024-08-17 22:17:30 -0400

    Update App Structure

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,6 @@
 import React,{createContext, useState} from 'react';
 import Header from './Header';
 import Features from './Features';
-import Background from './FeatureBackground';
 export const truthHook = createContext();
 
 export default function App() {
@@ -9,7 +8,6 @@ export default function App() {
   return (
     <truthHook.Provider value={{data,changeData}}>
      <Header></Header>
-     <Background></Background>
      <Features></Features>
     </truthHook.Provider>
   );
