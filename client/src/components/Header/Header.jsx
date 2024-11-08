7c35518c715729a9dd5e1e6f2dc4624273749517
akhilreddy6g <akhilreddyg21pa@gmail.com>
2024-11-08 00:52:36 -0500

    Adjust assets Path

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -9,17 +9,17 @@ export default function Header(){
     <div className={`mainBackground ${state.darkMode? "mainBackground1" : "mainBackground2"}`}></div>
     <nav className={`navbar navbar-expand-lg bg-body-tertiary ${state.darkMode && "navdarkMode"}`}>
     <div className="container-fluid">
-      <img className="logo" id="menulogo" src="client/public/assets/sidebar.svg" alt="Menu logo" onClick={() => {takeAction({type:"changeFthState"});}}/>
-      <Link to="/target"><button className="targetContainer"><img className="logo" id="dailyactlogo" src="client/public/assets/target.svg" alt="DAct logo" /></button></Link>
+      <img className="logo" id="menulogo" src="./assets/sidebar.svg" alt="Menu logo" onClick={() => {takeAction({type:"changeFthState"});}}/>
+      <Link to="/target"><button className="targetContainer"><img className="logo" id="dailyactlogo" src="./assets/target.svg" alt="DAct logo" /></button></Link>
       <div className="title-container" id="titlecontainer">
         <a className="navbar-brand" id="titlelink" href="/">
           <p id="titlename">Workspace</p>
         </a>
       </div>
       <DateTimeDisplay></DateTimeDisplay>
-      <img className="logo" id="daymodelogo" src={state.darkMode? "client/public/assets/moon.svg" :"client/public/assets/sun.svg"} onClick={() => {takeAction({type:"changeBgState"});}} alt="Day Mode" />
+      <img className="logo" id="daymodelogo" src={state.darkMode? "./assets/moon.svg" :"./assets/sun.svg"} onClick={() => {takeAction({type:"changeBgState"});}} alt="Day Mode" />
       <a href="/day-off">
-        <img className="logo" id="powerlogo" src="client/public/assets/power.svg" alt="Power logo"/>
+        <img className="logo" id="powerlogo" src="./assets/power.svg" alt="Power logo"/>
       </a>
     </div>
   </nav></>);
